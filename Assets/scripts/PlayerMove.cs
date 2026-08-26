996a81bac605d3d5aa1f1d62c7fbb3296b20046c
mitakanaparede <lufmeneghini@gmail.com>
2026-08-25 21:54:07 -0300

    Working

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float speed = 5;
+
+    public Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Keyboard.current[Key.W].isPressed) { direction += transform.forward; }
+
+        direction = Vector3.ClampMagnitude(direction, 1f);
+
+        rb.linearVelocity = new Vector3(direction.x * speed, rb.linearVelocity.y, direction.z * speed);
+    }
+}
+
